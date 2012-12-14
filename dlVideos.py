41700eefe8ba31b36cca9850c359c622f3c60207
drkitty <bumblingkitty@gmail.com>
2012-12-13 22:29:06 -0800

    dlVideos.py is functional. It now downloads the actual video to _video/$name and symlinks into the playlist directory. All symlinks should be checked every run, but I have yet to test this. Still working on error handling, although the basic functionality is there.

--- dlVideos.py
+++ dlVideos.py
@@ -7,77 +7,103 @@ import subprocess
 try:
 	videosJson_file = open("videos.json", "r")
 	videos = json.load(videosJson_file)
-	videosJson_file.close()
 	
 	playlistsJson_file = open("playlists.json", "r")
 	playlists = json.load(playlistsJson_file)
-	playlistsJson_file.close()
 except:
-	print "Please download the playlists first."
+	print "Please run getPlaylists.py first."
 	print "Exiting...."
 	exit()
+finally:
+	try:
+		videosJson_file.close()
+		playlistsJson_file.close()
+	except:
+		pass
 
 
 try:
 	done_file = open("done.json", "r")
 	done = json.load(done_file)
-	done_file.close()
 except:
 	done = []
-
-
-
-
+finally:
+	try:
+		done_file.close()
+	except:
+		pass
 
 
 try:
+	if not os.access("_videos", os.F_OK):
+		os.makedirs("_videos")
 	for thisPlaylist in playlists:
 		if not os.access(thisPlaylist["name"], os.F_OK):
 			os.makedirs(thisPlaylist["name"])
 	
 	for thisVideo in videos:
+		filePath = "_videos/" + thisVideo["title"]
+		
+		#'filter(func, list)' returns the item(s) in 'list' for which func(item) is True
+		#'lambda arg: foo' defines an anonymous (unnamed) function that takes 'arg' as an input and returns 'foo'
+		#so this lambda returns True or False depending on whether the key "idOrUrl" maps to the value thisVideo["pageUrl"]
+		#and this whole filter statement returns a list containing the item in 'playlists' that has the specified "idOrUrl"
+		#'[0]' (at the end of the line) selects the first (and only) item in the list
+		inPlaylist = filter(lambda pl: pl["idOrUrl"] == thisVideo["playlistIdOrUrl"], playlists)[0]
+		#thus, 'inPlaylist' is a dictionary describing the playlist in which 'thisVideo' resides
+		
+		#note that 'thisVideo' may be in multiple playlists, in which case it will have multiple entries in 'videos', one for each playlist
+		
 		if done.count(thisVideo["pageUrl"]) == 0:
 			print ""
 			print thisVideo["pageUrl"] + ":"
 			
-			#'filter(func, list)' returns item(s) in list for which func(item) is true
-			#'lambda arg: foo' defines an anonymous (unnamed) function that takes 'arg' as an input and returns 'foo'
-			#so this lambda returns True or False depending on whether the key "idOrUrl" maps to the value thisVideo["pageUrl"]
-			#and this whole filter statement returns a list containing the item in 'playlists' that has the specified "idOrUrl"
-			inPlaylist = filter( lambda pl: pl["idOrUrl"] == thisVideo["playlistIdOrUrl"], playlists )[0]
-			#'inPlaylist' is the playlist thisVideo is in
-			
-			#print inPlaylist
-			#print thisVideo
-			#exit()
-			
-			fileName = inPlaylist["name"] + "/" + thisVideo["title"]
-			print "Downloading to '" + fileName + "'"
+			print "Downloading to '" + filePath + "'"
 			
 			
 			myProcess = subprocess.Popen(['youtube-dl', '-g', thisVideo["pageUrl"]], stdout=subprocess.PIPE)
-			vidRawURL = myProcess.communicate()[0].rstrip("\n")
-			print vidRawURL
+			thisVidRawUrl = myProcess.communicate()[0].rstrip("\n")
 			
-
-			#f = open(fileName, "w")
-			#thing = urllib2.urlopen(thisVidRawURL)
-			#try:
-				#f.write(thing.read())
-			#except:
-				#print "fak u dolan."
-			#finally:
-				#f.close()
+			try:
+				f = open(filePath, "w")
+				thing = urllib2.urlopen(thisVidRawUrl)
+				f.write(thing.read())
+			except urllib2.HTTPError as e:
+				if e.code == 404:
+					print "WARNING: Video has been removed from YouTube or something like that."
+					print "WARNING: Skipping video...."
+					#not fatal!
+				elif e.code == 402:
+					print "FATAL ERROR: YouTube has apparently taken issue with the amount of bandwidth you're using."
+					print "FATAL ERROR: Wait a few minutes and try again."
+					print "FATAL ERROR: Exiting...."
+					raise
+				else:
+					print "FATAL ERROR: Unknown HTTP Error " + str(e.code)
+					raise
+			finally:
+				try:
+					f.close()
+					del f
+				except:
+					pass
 			
 			done.append(thisVideo["pageUrl"])
-		#<<<<  MAKE LINK TO VIDEO  >>>>
+		
+		symlinkPath = inPlaylist["name"] + "/" + thisVideo["title"]
+		if ( done.count(thisVideo["pageUrl"]) > 0 ) and ( not os.access(symlinkPath, os.R_OK) ):
+			symlinkTarget = "../_videos/" + thisVideo["title"]
+			print "Symlinking from '" + symlinkPath + "' to '" + symlinkTarget + "'...."
+			os.symlink(symlinkTarget, symlinkPath)
+except:
+	print "Terminating...."
+	raise
 finally:
-	#try:
 	done_file = open("done.json", "w")
 	json.dump(done, done_file)
+	done_file.close()
+	
+	#try:
+		#f.close()
 	#except:
-		#print "Couldn't write 'dl.json'."
-		#print "Looks like you're SOL."
-	#finally:
-		#if done_file != None:
-			#done_file.close()
+		#pass
